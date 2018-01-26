6f0c21aff4add1865af561854cacad1094d2a9e2
slaapz <slaapz@gmail.com>
2018-01-26 16:33:06 -0700

    Added images and colors to the format objects

--- object.js
+++ object.js
@@ -1,44 +1,44 @@
 const formats =[
   {
     img: "img/1.jpg",
-    h1Color: "",
-    h3Color: "",
-    h4Color: ""
+    h1Color: "chartreuse",
+    h3Color: "chartreuse",
+    h4Color: "aqua"
   },
   {
     img: "img/2.jpg",
-    h1Color: "",
-    h3Color: "",
-    h4Color: ""
+    h1Color: "orangered",
+    h3Color: "moccasin",
+    h4Color: "darkgreen"
   },
   {
     img: "img/3.jpg",
-    h1Color: "",
-    h3Color: "",
-    h4Color: ""
+    h1Color: "pink",
+    h3Color: "chocolate",
+    h4Color: "cyan"
   },
   {
     img: "img/4.jpg",
-    h1Color: "",
-    h3Color: "",
-    h4Color: ""
+    h1Color: "magenta",
+    h3Color: "yellow",
+    h4Color: "cyan"
   },
   {
     img: "img/5.jpg",
-    h1Color: "",
-    h3Color: "",
-    h4Color: ""
+    h1Color: "black",
+    h3Color: "darkslategrey",
+    h4Color: "white"
   },
   {
     img: "img/6.jpg",
-    h1Color: "",
-    h3Color: "",
-    h4Color: ""
+    h1Color: "black",
+    h3Color: "chartreuse",
+    h4Color: "cyan"
   },
   {
     img: "img/7.jpg",
-    h1Color: "",
-    h3Color: "",
-    h4Color: ""
+    h1Color: "darkslateblue",
+    h3Color: "magenta",
+    h4Color: "cyan"
   }
 ];
