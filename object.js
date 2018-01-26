e816b32853e7ec816d888aa7344deccd4be70bbf
slaapz <slaapz@gmail.com>
2018-01-26 15:56:01 -0700

    Added object.js which contains the different css to randomize

---
+++ object.js
@@ -0,0 +1,44 @@
+const formats =[
+  {
+    img: "img/1.jpg",
+    h1Color: "",
+    h3Color: "",
+    h4Color: ""
+  },
+  {
+    img: "img/2.jpg",
+    h1Color: "",
+    h3Color: "",
+    h4Color: ""
+  },
+  {
+    img: "img/3.jpg",
+    h1Color: "",
+    h3Color: "",
+    h4Color: ""
+  },
+  {
+    img: "img/4.jpg",
+    h1Color: "",
+    h3Color: "",
+    h4Color: ""
+  },
+  {
+    img: "img/5.jpg",
+    h1Color: "",
+    h3Color: "",
+    h4Color: ""
+  },
+  {
+    img: "img/6.jpg",
+    h1Color: "",
+    h3Color: "",
+    h4Color: ""
+  },
+  {
+    img: "img/7.jpg",
+    h1Color: "",
+    h3Color: "",
+    h4Color: ""
+  }
+];
