5ce00c3d2bbc8b715dcd8e5bea6b2f077f257805
slaapz <slaapz@gmail.com>
2018-01-26 17:23:40 -0700

    test commit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // Display local time, greeting, and message and update continuously.
 
 // Set variables
+const objectIndex = Math.floor((Math.random() * 7));
+console.log(objectIndex);
+const cssFormat = formats[objectIndex];
 const elTime = document.getElementById('time'),
       elGreeting = document.getElementById('greeting'),
-      elMessage = document.getElementById('msg');
+      elMessage = document.getElementById('msg'),
+      elHeader = document.querySelector('header');
+
 
 // Default greetings messages
 const h = new Date().getHours();
+changeCSS(cssFormat);
 updateGreeting(h);
 
 const myTime = setInterval(myClock,1000),
@@ -30,12 +36,20 @@ function updateGreeting(hours){
     elGreeting.innerHTML = 'Rise and shine, Pat.';
     elMessage.innerHTML = 'What are we doing this morning?';
   } else if(hours >=12 && hours < 17){
-    elGreeting.innerHTML = 'It a bright beautiful day, Pat.';
+    elGreeting.innerHTML = 'It\'s a bright beautiful day, Pat.';
     elMessage.innerHTML = 'What are we doing this afternoon?';
   } else {
     elGreeting.innerHTML = 'Shut \'er down, Pat.';
     elMessage.innerHTML = 'What\'s going on this evening?';
   }
+}
+
+function changeCSS(cssObj){
+  console.log(cssObj);
+  elHeader.style.backgroundImage = `url('${cssObj.img}'`;
+  elTime.style.color=`${cssObj.h1Color}`;
+  elGreeting.style.color=`${cssObj.h3Color}`;
+  elMessage.style.color=`${cssObj.h4Color}`;
 }
 
 
